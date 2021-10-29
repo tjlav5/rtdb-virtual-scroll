7ae44f2fcdd7601e68fa532c73ee8c40a2cf4198
TJ Lavelle <tj@lavelle.io>
2021-10-29 01:05:11 +0000

    Revert to consolidated editor-node-type

--- projects/demo/src/app/rtdb-viewer/rtdb-viewer.module.ts
+++ projects/demo/src/app/rtdb-viewer/rtdb-viewer.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RtdbViewerComponent, RtdbViewerToggleComponent } from './rtdb-viewer.component';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RtdbEditorChildNodeComponent, RtdbEditorLeafNodeComponent, RtdbEditorNodeComponent, RtdbRealtimeNodeComponent, RtdbSaveNodeComponent } from './rtdb-node.component';
+import { RtdbEditorNodeComponent, RtdbRealtimeNodeComponent, RtdbSaveNodeComponent } from './rtdb-node.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, ScrollingModule],
-  declarations: [RtdbEditorNodeComponent, RtdbViewerToggleComponent, RtdbSaveNodeComponent, RtdbRealtimeNodeComponent, RtdbEditorChildNodeComponent, RtdbEditorLeafNodeComponent, RtdbViewerComponent],
+  declarations: [RtdbEditorNodeComponent, RtdbViewerToggleComponent, RtdbSaveNodeComponent, RtdbRealtimeNodeComponent, RtdbViewerComponent],
   exports: [RtdbViewerComponent],
 })
 export class RtdbViewerModule { }
